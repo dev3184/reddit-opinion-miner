e1dc25b8c625765d5bafc50fd2ba39f94ccd0163
Mit Bambhroliya <119504702+MitBambhroliya@users.noreply.github.com>
2025-05-05 13:02:06 +0530

    Update missing_data_handle.py

--- missing_data_handle.py
+++ missing_data_handle.py
@@ -8,6 +8,6 @@ df = df[df["Cleaned_Text"].notna()]  # Removes NaN values
 df = df[df["Cleaned_Text"].str.strip() != ""]  # Removes empty strings
 
 # Save the cleaned dataset
-df.to_csv("cleaned_reddit_data_2.csv", index=False)
+df.to_csv("Your cleaned dataset location to save", index=False)
 
 print("Removed empty comments. Cleaned dataset saved as cleaned_sentiment_data_no_empty_comments.csv")
